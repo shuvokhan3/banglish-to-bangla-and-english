fb988dd8a74e88d84faa9beebf4daebc63c57b69
shuvokhan3 <shuvoj360@gmail.com>
2025-12-02 18:36:37 +0600

    Modify on User and readme file

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,11 +9,10 @@ class User extends Model
 {
     use HasFactory;
     protected $table = 'users';
-    protected $fillable= ['name', 'email', 'email_verified_at', 'password', 'otp'];
+    protected $fillable= ['email', 'email_verified_at', 'password', 'otp'];
 
     protected $attributes = [
         'otp' => '0',
-        'email_verified_at'=>'0'
     ];
 
     public function conversations(): \Illuminate\Database\Eloquent\Relations\HasMany
@@ -21,7 +20,9 @@ class User extends Model
         return $this->hasMany(Conversation::class);
     }
 
-
-
+    public function UserDetails(): \Illuminate\Database\Eloquent\Relations\HasOne
+    {
+        return $this->HasOne(UserDetails::class);
+    }
 
 }
